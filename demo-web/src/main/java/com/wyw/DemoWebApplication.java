ccc3737f65df30089597f1d1223ca657fd37765b
yx_wyw <wyw376410272@qq.com>
2020-01-15 21:59:09 +0800

    123

--- demo-web/src/main/java/com/wyw/DemoWebApplication.java
+++ demo-web/src/main/java/com/wyw/DemoWebApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.wyw.dao")
-@SpringBootApplication
+@MapperScan("com.wyw.dao")// 扫描dao层mapper的位置
+@SpringBootApplication(scanBasePackages = "com.wyw")// 全局扫描
 public class DemoWebApplication {
 
     public static void main(String[] args) {
